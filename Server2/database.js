552cadbe88e29616e43092ea987c4a61620b707f
FernandoArmira <2864934620101@ingenieria.usac.edu.gt>
2021-09-14 03:04:30 -0600

    Server 2

---
+++ Server2/database.js
@@ -0,0 +1,87 @@
+//Funciones Base de datos
+
+//var dato = "";
+var fecha = new Date();
+var totaldatos = 0;
+
+//Conexion a BD
+//mongodb+srv://fernando:<password>@cluster0.tk4g5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+//mongodb://fernando:<password>@cluster0-shard-00-00.tk4g5.mongodb.net:27017,cluster0-shard-00-01.tk4g5.mongodb.net:27017,cluster0-shard-00-02.tk4g5.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-zppbt4-shard-0&authSource=admin&retryWrites=true&w=majority
+const MongoClient = require('mongodb').MongoClient;
+//const url = "mongodb+srv://fernando:ChZIc4DP7SwmWED1@cluster0.tk4g5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const url = "mongodb://fernando:ChZIc4DP7SwmWED1@cluster0-shard-00-00.tk4g5.mongodb.net:27017,cluster0-shard-00-01.tk4g5.mongodb.net:27017,cluster0-shard-00-02.tk4g5.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-zppbt4-shard-0&authSource=admin&retryWrites=true&w=majority";
+console.log("Conexion a BD")
+
+
+module.exports = {
+
+    //Funcion insertar datos en la DB
+insertData: function(data){
+    MongoClient.connect(url,function(err, db){
+        if (err) throw err;
+        const dbo = db.db('mydb');
+        const obj  = JSON.parse(data);
+        dbo.collection('medidas').insertOne(obj, function(err,res){
+            if(err) throw err;
+            db.close();
+        });
+    });
+},
+
+//Agregar fecha y hora del sistema
+datetime: function(data){
+    const str = data.substring(0, data.length - 1);
+    //console.log(str);
+    const str2 = str + ", \"fecha\": \""  + fecha.getDate() + "-" + (fecha.getMonth()+1) + "-" + fecha.getUTCFullYear() + "\", \"hora\": \"" + fecha.getHours() + ":" + fecha.getMinutes() + ":" + fecha.getSeconds() + "\"}"
+    console.log(str2);
+    return str2
+},
+
+//Filtrar datos
+
+//Seleccionar un dato en especifico
+select: function (req, res, datep, i1, i2){
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        dbo.collection("medidas").find({fecha: datep}).skip(i1).limit(i2).toArray(function(err, result) {
+          if (err) throw err;
+          //console.log(result);
+          res.send(result);
+          db.close();
+        });
+      });
+},
+
+
+    // Seleccionar todos los datos de un dia
+selectData: function(req, res, datep){
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        dbo.collection("medidas").find({fecha: datep}).toArray(function(err, result) {
+          if (err) throw err;
+          //console.log(result);
+          res.send(result);
+          db.close();
+        });
+      });
+},
+
+    // Numero de registros en el dia
+    selectCount: function (datap){
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("mydb");
+            dbo.collection("medidas").find({fecha:datap}).count({}, function(err, result){
+                if(err){
+                    console.log("Error")
+                }else{
+                    console.log(result)
+                    totaldatos = result
+                }
+            })
+          });
+
+    }
+}
---
+++ Server2/index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+
+const app = express()
+
+const database = require('./database.js');
+
+app.set('port', 3000)
+
+app.listen(app.get('port'), () => {
+    console.log(`Servidor corriendo en el puerto ${app.get('port')}`)
+})
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Conexion a puerto serial
+const SerialPort = require('serialport');
+const ReadLine  = require('@serialport/parser-readline');
+
+//Asignar el numero de puerto serial y baudrate
+const port = new SerialPort('COM4',{baudRate: 9600});
+const parser = port.pipe(new ReadLine({delimiter: '\n'}));
+
+//Lectura de datos enviados de arduino
+port.on("open",() => {
+    console.log('Se abrio la comunicacion');
+});
+
+parser.on("data", data =>{
+    console.log(data);
+    database.insertData(database.datetime(data))
+});
+
+
+// Rutas
+app.get('/',(req, res ) => {
+    res.send("Proyecto 1 Arqui 2")
+})
+
+app.get('/dato',(req, res ) => {
+    database.select(req,res, "13-9-2021", 0, 1)
+})
+
+app.get('/datosdia',(req, res ) => {
+    database.selectData(req,res, "13-9-2021")
+})
+
+//database.selectCount('12-9-2021');
+
+//Datos de prueba
+
+/*
+// Ingresar datos de prueba a la DB
+prueba = "{\"sentado\": 0, \"peso\": 150}";
+console.log(prueba);
+database.insertData(database.datetime(prueba))
+*/
+
