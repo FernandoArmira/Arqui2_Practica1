6ab10ab915c3f072d3fbafec830872023ae71acb
Aroche <2708501980101@ingenieria.usac.edu.gt>
2021-09-17 11:42:17 -0600

    FrontEnd

--- FrontEnd/smart-chair/src/App.jsx
+++ FrontEnd/smart-chair/src/App.jsx
@@ -3,27 +3,37 @@ import { Container } from "@material-ui/core";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import { Configuracion, Reportes, Dashboard, Monitoreo, Home } from "./pages";
 import { NavBar, Menu } from "./components";
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import { URL_API_BACKEND } from "./config";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
+    display: "flex",
   },
   appBarSpacer: theme.mixins.toolbar,
   content: {
     flexGrow: 1,
-    height: '100vh',
-    overflow: 'auto',
+    height: "100vh",
+    overflow: "auto",
   },
   container: {
     paddingTop: theme.spacing(4),
     paddingBottom: theme.spacing(4),
-  }
+  },
 }));
 
-
 export const App = () => {
+  const [dataMonitor, setDataMonitor] = React.useState([]);
+  const [dataDashboard, setDataDashboard] = React.useState([]);
+  const [dataDashboardTotalTime, setDataDashboardTotalTime] = React.useState(
+    []
+  );
+  const [dataDashboardTTime, setDataDashboardTTime] = React.useState([]);
+  const [dataDashboardPromedioUseTime, setDataDashboardPromedioUseTime] =
+    React.useState([]);
+  const [dataDashboardPromedioLevanta, setDataDashboardPromedioLevanta] =
+    React.useState([]);
   const classes = useStyles();
   const [open, setOpen] = React.useState(true);
   const handleDrawerOpen = () => {
@@ -32,33 +42,95 @@ export const App = () => {
   const handleDrawerClose = () => {
     setOpen(false);
   };
+
+  const getDataMonitor = async () => {
+    const response = await fetch(`${URL_API_BACKEND}/monitoreo`);
+    const allData = await response.json();
+    setDataMonitor(allData);
+  };
+
+  const getDataDashboard = async () => {
+    const response = await fetch(`${URL_API_BACKEND}/peso`);
+    const allData = await response.json();
+    setDataDashboard(allData);
+  };
+
+  const getDataDashboardTotalTime = async () => {
+    const response = await fetch(`${URL_API_BACKEND}/tiempouso`);
+    const allData = await response.json();
+    setDataDashboardTotalTime(allData);
+  };
+
+  const getDataDashboardTTime = async () => {
+    const response = await fetch(`${URL_API_BACKEND}/totaltiempo`);
+    const allData = await response.json();
+    setDataDashboardTTime(allData);
+  };
+
+  const getDataDashboardPromedioUseTime = async () => {
+    const response = await fetch(`${URL_API_BACKEND}/promediouso`);
+    const allData = await response.json();
+    setDataDashboardPromedioUseTime(allData);
+  };
+
+  const getDataDashboardPromedioLevanta = async () => {
+    const response = await fetch(`${URL_API_BACKEND}/promediolevantadas`);
+    const allData = await response.json();
+    setDataDashboardPromedioLevanta(allData);
+  };
+
+  React.useEffect(() => {
+    // const interval = setInterval(() => {
+    // }, 90000);
+    getDataMonitor().catch((error) => console.error(error));
+    getDataDashboard().catch((error) => console.error(error));
+    getDataDashboardTotalTime().catch((error) => console.error(error));
+    getDataDashboardPromedioUseTime().catch((error) => console.error(error));
+    getDataDashboardTTime().catch((error) => console.error(error));
+    getDataDashboardPromedioLevanta().catch((error) => console.error(error));
+
+    // return () => clearInterval(interval);
+  }, []);
+
   return (
     <Router>
       <div className={classes.root}>
-      <NavBar open={open} handleDrawerOpen={handleDrawerOpen}/>
-      <Menu open={open} handleDrawerClose={handleDrawerClose}/>
-      <main className={classes.content}>
-        <div className={classes.appBarSpacer} />
-      <Container className={classes.container} maxWidth="lg">
-        <Switch>
-          <Route exact path="/configuracion">
-            <Configuracion />
-          </Route>
-          <Route exact path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route exact path="/monitoreo">
-            <Monitoreo />
-          </Route>
-          <Route exact path="/reportes">
-            <Reportes />
-          </Route>
-          <Route exact path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Container>
-      </main>
+        <NavBar open={open} handleDrawerOpen={handleDrawerOpen} />
+        <Menu open={open} handleDrawerClose={handleDrawerClose} />
+        <main className={classes.content}>
+          <div className={classes.appBarSpacer} />
+          <Container className={classes.container} maxWidth="lg">
+            <Switch>
+              <Route exact path="/configuracion">
+                <Configuracion />
+              </Route>
+              <Route exact path="/dashboard">
+                <Dashboard
+                  dataDashboard={dataDashboard}
+                  dataDashboardTTime={dataDashboardTTime}
+                  dataDashboardTotalTime={dataDashboardTotalTime}
+                  dataDashboardPromedioUseTime={dataDashboardPromedioUseTime}
+                  dataDashboardPromedioLevanta={dataDashboardPromedioLevanta}
+                />
+              </Route>
+              <Route exact path="/monitoreo">
+                <Monitoreo data={dataMonitor} />
+              </Route>
+              <Route exact path="/reportes">
+                <Reportes
+                  dataDashboard={dataDashboard}
+                  dataDashboardTTime={dataDashboardTTime}
+                  dataDashboardTotalTime={dataDashboardTotalTime}
+                  dataDashboardPromedioUseTime={dataDashboardPromedioUseTime}
+                  dataDashboardPromedioLevanta={dataDashboardPromedioLevanta}
+                />
+              </Route>
+              <Route exact path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </Container>
+        </main>
       </div>
     </Router>
   );
--- FrontEnd/smart-chair/src/pages/Dashboard.jsx
+++ FrontEnd/smart-chair/src/pages/Dashboard.jsx
@@ -1,5 +1,242 @@
 import React from "react";
+import { useTheme } from "@material-ui/core/styles";
+import { Line, Pie, Bar } from "react-chartjs-2";
+import Grid from "@material-ui/core/Grid";
 
-export const Dashboard = () => {
-    return <></>;
-}
+import { Typography, Card, CardContent, IconButton } from "@material-ui/core";
+
+const options = {
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
+  },
+};
+
+export const Dashboard = ({
+  dataDashboard = [],
+  dataDashboardTotalTime = [],
+  dataDashboardPromedioUseTime = [],
+  dataDashboardTTime = [],
+  dataDashboardPromedioLevanta = [],
+}) => {
+  const primerosDiez = dataDashboard.slice(0, 10);
+  const data = {
+    labels: dataDashboard.map(({ fecha }) => fecha),
+    datasets: [
+      {
+        label: "Peso",
+        data: dataDashboard.map(({ peso }) => peso),
+        fill: false,
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgba(255, 99, 132, 0.2)",
+      },
+    ],
+  };
+
+  const datapie = {
+    labels: dataDashboardTotalTime
+      .filter((row) => row !== null)
+      .map(({ dia }) => dia),
+    datasets: [
+      {
+        label: "Cantidad de Uso",
+        data: dataDashboardTotalTime
+          .filter((row) => row !== null)
+          .map(({ tiempototal }) => tiempototal),
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(211, 211, 211, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+          "rgba(211, 211, 200, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const dataBar = {
+    labels: dataDashboardPromedioLevanta
+      .filter((row) => row !== null)
+      .map(({ dia }) => dia),
+    datasets: [
+      {
+        label: "Promedio de veces que se levanta.",
+        data: dataDashboardPromedioLevanta
+          .filter((row) => row !== null)
+          .map(({ promediolevantadas }) => promediolevantadas),
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(211, 211, 211, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+          "rgba(211, 211, 200, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return (
+    <>
+      <Grid container spacing={3}>
+        <Grid item md={4} lg={6}>
+          <Typography
+            component="h1"
+            variant="h5"
+            color="inherit"
+            noWrap
+            align="center"
+          >
+            Total Tiempo en Uso: {dataDashboardTTime.tiempototal}
+          </Typography>
+        </Grid>
+
+        {/* <Grid item md={4} lg={6}>
+          <Typography
+            component="h1"
+            variant="h5"
+            color="inherit"
+            noWrap
+            align="center"
+          >
+            Tiempo de uso promedio por dia: {dataDashboardPromedioUseTime.promediotiempo}
+          </Typography>
+        </Grid> */}
+      </Grid>
+
+      <br></br>
+
+      <Typography
+        component="h1"
+        variant="h3"
+        color="inherit"
+        noWrap
+        align="center"
+      >
+        Tendencia del Peso
+      </Typography>
+
+      <br></br>
+
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Line data={data} options={options} />
+        </Grid>
+      </Grid>
+
+      <br></br>
+
+      <Typography
+        component="h1"
+        variant="h3"
+        color="inherit"
+        noWrap
+        align="center"
+      >
+        Dias de Mayor Uso
+      </Typography>
+
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Pie data={datapie} />
+        </Grid>
+      </Grid>
+
+      <br></br>
+
+      <Typography
+        component="h1"
+        variant="h3"
+        color="inherit"
+        noWrap
+        align="center"
+      >
+        Promedio de veces que se levanta
+      </Typography>
+
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Bar data={dataBar} options={options} />
+        </Grid>
+      </Grid>
+
+      <Typography
+        component="h1"
+        variant="h3"
+        color="inherit"
+        noWrap
+        align="center"
+      >
+        Uso promedio por dia
+      </Typography>
+
+      <br></br>
+
+      <Grid container spacing={3}>
+        {dataDashboardPromedioUseTime
+          .filter((row) => row !== null)
+          .map((data) => (
+            <Grid item xs={12} md={4} lg={6} key={`carddash-${data.dia}`}>
+              <Card>
+                <CardContent>
+                  <Typography
+                    component="p"
+                    align="center"
+                    component="h1"
+                    variant="h5"
+                  >
+                    {(data.dia == "Mon" && "Lunes - (H)") ||
+                      (data.dia == "Sun" && "Domingo - (H)") ||
+                      (data.dia == "Tue" && "Martes - (H)") ||
+                      (data.dia == "Wed" && "Miercoles - (H)") ||
+                      (data.dia == "Thu" && "Jueves - (H)") ||
+                      (data.dia == "Fri" && "Viernes - (H)") ||
+                      (data.dia == "Sat" && "Sabado - (H)")}
+                  </Typography>
+                </CardContent>
+                <div align="center" size="large">
+                  <Typography
+                    component="p"
+                    align="center"
+                    component="h1"
+                    variant="h5"
+                  >
+                    {data.tiempopromedio}
+                  </Typography>
+                </div>
+              </Card>
+            </Grid>
+          ))}
+      </Grid>
+      <br></br>
+    </>
+  );
+};
